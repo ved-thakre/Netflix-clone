ebc1e12080d23e5ce1283ced3cb8f764ac500a7f
ved-thakre <vedantthakre7@gmail.com>
2023-08-14 21:11:47 +0530

    first-commit

---
+++ src/Components/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar'
+import './HomeScreen.css'
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBaXOawVmJJHP43S1HR1hzX_QCGZboEQIA",
+  authDomain: "netflix-87397.firebaseapp.com",
+  projectId: "netflix-87397",
+  storageBucket: "netflix-87397.appspot.com",
+  messagingSenderId: "494104901913",
+  appId: "1:494104901913:web:c187331926decebbb16d88"
+};
